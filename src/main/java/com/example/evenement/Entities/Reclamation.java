2f537f6b8be7847a6b739dfc696b85247875fd81
Ahmed Belkahia <ahmed.belkahia@esprit.tn>
2023-10-24 00:46:21 +0100

    aad

---
+++ src/main/java/com/example/evenement/Entities/Reclamation.java
@@ -0,0 +1,33 @@
+package com.example.evenement.Entities;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Document(collection = "reclamation")
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+
+
+
+public class Reclamation implements Serializable {
+    @Id
+    @GeneratedValue
+
+    private int id;
+    private String nom;
+    private String prenom;
+    private String reclamation;
+
+
+}
---
+++ src/main/java/com/example/evenement/Repositories/IReclamationRepo.java
@@ -0,0 +1,10 @@
+package com.example.evenement.Repositories;
+
+import com.example.evenement.Entities.Reclamation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IReclamationRepo extends MongoRepository<Reclamation,Integer> {
+}
