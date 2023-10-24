775042b2a1b78491c00e3f77e714e9d8fbc11ab4
HamoudaYoussef <123754179+HamoudaYoussef@users.noreply.github.com>
2023-10-24 04:30:27 +0100

    finish

---
+++ events-ahmedt/src/main/java/com/example/evenement/entities/Ticket.java
@@ -0,0 +1,27 @@
+package com.example.evenement.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket implements Serializable {
+    private static final long serialVersionUID = 6;
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nom,prenom,email;
+
+
+}
