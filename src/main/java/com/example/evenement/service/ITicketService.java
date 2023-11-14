bb1280c96d966c1dbc3feb0617995fe28fd55ed8
youssefhamouda <ahmed.tijani@esprit.tn>
2023-11-14 09:43:04 +0100

    rmi

--- src/main/java/com/example/evenement/service/ITicketService.java
+++
@@ -1,9 +0,0 @@
-package com.example.evenement.service;
-
-import com.example.evenement.entities.Ticket;
-
-public interface ITicketService {
-    Ticket addTicket(Ticket ticket);
-    Ticket updateTicket(int id, Ticket newUser);
-    String deleteTicket(int id);
-}
